20f77a56755e81e2e354e15e191df2e2b8a12ff0
Sylar Li <65619327+OwOsas@users.noreply.github.com>
2021-10-19 11:50:17 -0400

    First Push

---
+++ Beta/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="landing_container">
+        <div id="title_container">
+            <div id="title_deco"></div>
+            <div id="title">The Elder Scroll Cookbook</div>
+        </div>
+
+        
+    </div>
+</body>
+</html>
